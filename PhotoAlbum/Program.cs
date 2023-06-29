1fdaf492bbce6fad6c3d533b01dd096a08de709e
Milorad <mfelbapov@gmail.com>
2023-06-29 17:36:54 -0500

    Setup app start

--- PhotoAlbum/Program.cs
+++ PhotoAlbum/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PhotoAlbum;
+using PhotoAlbum.ApiClient;
+using PhotoAlbum.Services;
 
+var httpClient = new HttpClient();
+var apiClient = new AlbumApiClient(httpClient);
+var albumService = new AlbumService(apiClient);
+var consoleWrapper = new ConsoleWrapper();
+var consolePrompt = new Prompt(albumService, consoleWrapper);
+await consolePrompt.Run();
