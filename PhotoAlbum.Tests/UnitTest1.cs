904b6991210c0e67a900dbaa62d50e640c350b48
Milorad <mfelbapov@gmail.com>
2023-06-29 09:12:10 -0500

    Initial commit, create source and test project, gitignore and rename to main branch

---
+++ PhotoAlbum.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace PhotoAlbum.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
